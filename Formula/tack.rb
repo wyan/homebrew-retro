1abd9552c1b0ed1949867cf09f80cc24e77f9e5b
Alice Wyan <finitud@gmail.com>
2020-06-03 20:42:27 +0200

    Add formula for The Amsterdam Compiler Collection (tack)

---
+++ Formula/tack.rb
@@ -0,0 +1,17 @@
+class Tack < Formula
+  desc "The Amsterdam Compiler Kit"
+  homepage "http://tack.sourceforge.net"
+  url "https://github.com/wyan/ack/archive/homebrew-0.1.tar.gz"
+  version "2020.06.03"
+  sha256 "8f7104ca8bf45af015c9aec39dbfa109db471ce5b4f1db2427f57709f3117827"
+
+  def install
+    ENV.deparallelize
+    system "make", "PREFIX=#{prefix}"
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    system "false"
+  end
+end
